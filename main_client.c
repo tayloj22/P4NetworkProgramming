127e96d35a90165dcdac9365ca8b889a5db27112
Luke <kurlanl1@tcnj.edu>
2020-05-03 07:50:17 -0400

    aborting previous vers for jack's colors

--- main_client.c
+++ main_client.c
@@ -8,6 +8,7 @@
 #include <arpa/inet.h>
 #include <netdb.h> 
 #include <pthread.h>
+#include <ctype.h>
 
 #define PORT_NUM 1004
 
@@ -20,6 +21,130 @@ void error(const char *msg)
 typedef struct _ThreadArgs {
 	int clisockfd;
 } ThreadArgs;
+
+// LUKE
+// When user enters a room number for the third command line
+	// arguments, this function will work in tandum with
+	// main_server.listen_for_chat room to join the chat room
+	// and print whether the join was successful or not.
+void join_particular_room(int sockfd, int room_number) 
+{
+	// Send the message requesting to join a particular room.
+		// Will simply send one char: the room number.
+	char buffer[1];
+	memset(buffer, 0, 1);
+	buffer[0] = room_number + '0';
+	int n = send(sockfd, buffer, strlen(buffer), 0);
+	if (n < 0) error("ERROR writing to socket");
+	
+	// Recieve the room number back or a 'F' for failure.
+	memset(buffer, 0, 1);
+	n = recv(sockfd, buffer, 512, 0);
+	if (n < 0) error("ERROR recv() failed");
+	
+	// Print the result of join operation.
+	printf("-------------------------------------------\n");
+	if (buffer[0] != 'F') 
+		printf("You have been assigned the chat room number %s\n", buffer);
+	else 
+		printf("That chat room was full. Failure.\n");
+	printf("-------------------------------------------\n");	
+}
+
+// LUKE
+// When user does not enter anything for the third command line
+	// arguments, this function will work in tandum with
+	// main_server.listen_for_chat room to retrieve the available
+	// chat rooms. It will then ask the user which room it wants to
+	// join, or if the user would like to simply perform standard 
+	// broadcast procedures. 
+void retrieve_chat_rooms(int sockfd) 
+{
+	// Print the options from server.
+	printf("-------------------------------------------\n");
+	printf("Server says the following rooms are available.\n");
+	// FIXME: List the current chat rooms from server.
+	printf("-------------------------------------------\n");
+	
+	// Print and get the options for user selection.
+	printf("Enter 's' to NOT use a chatroom and use the standard");
+	printf(" broadcast procedures. Else, enter the room number.\n");
+	// FIXME: Get user input. Either an 's' or a chat number.
+	char buffer[1];
+	memset(buffer, 0, 1);
+	
+	// Simulations for testing purposes.
+	//buffer[0] = '1';		// Join chat room 1
+	buffer[0] = 's';		// Perform standard broadcasting
+
+	// If user entered an 's', we send over the 's' to indicate standard
+		// broadcast procedures.
+	if (buffer[0] == 's') {
+		int n = send(sockfd, buffer, strlen(buffer), 0);
+		if (n < 0) error("ERROR writing to socket");
+	}
+	// Otherwise the user entered a number and we request to join that room.
+	else {
+		join_particular_room(sockfd, atoi(&buffer[0]));
+	}
+}
+
+// LUKE
+// When user enters 'new' for the third command line
+	// arguments, this function will work in tandum with
+	// main_server.listen_for_chat room to join a new chat room
+	// and print whether the join was successful or not.
+void request_new_room(int sockfd) 
+{
+	// Send the message requesting a new room.
+		// Will simply send one char: 'n'.
+	char buffer[1];
+	memset(buffer, 0, 1);
+	buffer[0] = 'n';
+	int n = send(sockfd, buffer, strlen(buffer), 0);
+	if (n < 0) error("ERROR writing to socket");
+
+	// Receieve the chat room number back or an'F' for failure.
+	memset(buffer, 0, 1);
+	n = recv(sockfd, buffer, 512, 0);
+	if (n < 0) error("ERROR recv() failed");
+	
+	// Print the result of operation.
+	printf("-------------------------------------------\n");
+	if (buffer[0] != 'F')
+		printf("You have been assigned the chat room number %s\n", buffer);
+	else
+		printf("That chat room was full. Failure.\n");
+	printf("-------------------------------------------\n");	
+}
+
+// LUKE
+// Main function to make intelligent decisions based upon the third
+	// command line arguments. There are three paths to take:
+		// 1. User enters no third argument: retrieve_chat_room
+		// 2. User enters 'new': request_new_room
+		// 3. User enters [int]: join_particular_room
+void address_chat_room(char *arg3, int sockfd) 
+{
+	sleep(1);	// Wait for the server to catch up to client.
+
+	// Call worker functions based upon contents are third arg.
+	
+	if (arg3 == NULL) { // No third arg.
+		retrieve_chat_rooms(sockfd);
+	}
+	else if (strcmp(arg3, "new") == 0) { // 'new' is third arg.
+		request_new_room(sockfd);
+	}
+	else { // Third arg is numeric.
+		for (int i = 0; i < strlen(arg3); i++) {
+			if (isdigit(arg3[i]) == 0) { 
+				error("Invalid third argument");	
+			}
+		}
+		join_particular_room(sockfd, atoi(arg3));
+	}
+}
 
 void* thread_main_recv(void* args)
 {
@@ -39,6 +164,9 @@ void* thread_main_recv(void* args)
 		if (n < 0) error("ERROR recv() failed");
 
 		printf("\n%s\n", buffer);
+		// LUKE
+		printf("\nYou may now enter a message to the other users. ");
+		printf("Sending a blank message will quit.\n");
 	} while (n > 0);
 
 	return NULL;
@@ -114,10 +242,12 @@ int main(int argc, char *argv[])
 
 	// Send the username to the server to be stored
 	int n = send(sockfd, username, strlen(username), 0);
-	if (n < 0) {
-		error("ERROR writing to socket");
-	}
+	if (n < 0) { error("ERROR writing to socket"); }
 
+	// LUKE
+	// Complex function to address all chat room needs.
+		// Please read function documentation above.
+	address_chat_room(argv[2], sockfd);
 
 	// Split into two threads
 	pthread_t tid1;
@@ -135,7 +265,8 @@ int main(int argc, char *argv[])
 	args->clisockfd = sockfd;
 	pthread_create(&tid2, NULL, thread_main_recv, (void*) args);
 
-	// parent will wait for sender to finish (= user stop sending message and disconnect from server)
+	// parent will wait for sender to finish 
+		// user stop sending message and disconnect from server)
 	pthread_join(tid1, NULL);
 
 	close(sockfd);
--- main_server.c
+++ main_server.c
@@ -7,8 +7,17 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <ctype.h>
 
 #define PORT_NUM 1004
+
+// LUKE
+#define CHAT_ROOM_NUM 5
+#define CLIENTS_PER_ROOM 6
+
+// LUKE
+// Each index hold the number of clients in that chat room.
+int chatroom[CHAT_ROOM_NUM];
 
 void error(const char *msg)
 {
@@ -20,16 +29,21 @@ typedef struct _USR {
 	int clisockfd;		// socket file descriptor
 	char* username;     // client username
 	struct _USR* next;	// for linked list queue
+	// LUKE
+	int chatroom;	// -1 if not in a room, else the room number.
 } USR;
 
 USR *head = NULL;
 USR *tail = NULL;
 
-void add_tail(int newclisockfd, char* newname)
+void add_tail(int newclisockfd, char* newname, int chat_status)
 {
 	if (head == NULL) {
 		head = (USR*) malloc(sizeof(USR));
 		head->clisockfd = newclisockfd;
+		// LUKE
+		head->chatroom = chat_status;
+
 		head->username = (char*) malloc(strlen(newname) * sizeof(char));
 		strcpy(head->username, newname);
 		head->next = NULL;
@@ -37,6 +51,9 @@ void add_tail(int newclisockfd, char* newname)
 	} else {
 		tail->next = (USR*) malloc(sizeof(USR));
 		tail->next->clisockfd = newclisockfd;
+		// LUKE
+		tail->next->chatroom = chat_status;
+		
 		tail->next->username = (char*) malloc(strlen(newname) * sizeof(char));
 		strcpy(tail->next->username, newname);
 		tail->next->next = NULL;
@@ -75,7 +92,7 @@ void delete_client(int clisockfd)
 void print_list() {
 	printf("-------------------------------------------------------------------------\n");
 	printf("All currently connected clients are listed below:\n");
-	printf("[File Descriptor] : [username]\n");
+	printf("\t[FD] : [username]\n");
 	USR* cur = head;
 	while (cur != NULL) {
 		printf("\t[%d] : [%s]\n", cur->clisockfd, cur->username);
@@ -90,10 +107,10 @@ void print_list() {
 // Call function to delete client from list and print new client list.
 void delete_routine(int clisockfd) {
 	delete_client(clisockfd);
-	printf("A client chose to disconnect.\n");
 	print_list();
 }
 
+// Broadcast a message to ALL clients.
 void broadcast(int fromfd,  char* message)
 {
 	// figure out sender address
@@ -126,7 +143,7 @@ void broadcast(int fromfd,  char* message)
 			char buffer[512];
 
 			// prepare message
-			sprintf(buffer, "%s [%s]:%s", username, inet_ntoa(cliaddr.sin_addr), message);
+			sprintf(buffer, "[%s] : [%s] : %s", username, inet_ntoa(cliaddr.sin_addr), message);
 			int nmsg = strlen(buffer);
 
 			// send!
@@ -141,7 +158,139 @@ void broadcast(int fromfd,  char* message)
 typedef struct _ThreadArgs {
 	int clisockfd;
 	char* username;
+	// LUKE
+	int chat_status;
 } ThreadArgs;
+
+// LUKE
+// Search for an empty chatroom and return its number. 
+	// Return -1 if all full.
+int create_new_chatroom() 
+{
+	// Search for empty chatroom.
+	int i = 0;
+	while (i < CHAT_ROOM_NUM && chatroom[i] != 0) i++;
+	// Return -1 if no empty room exists.
+	if (i == CHAT_ROOM_NUM) 
+		return -1;
+	// Otherwise, increment the number of clients and return.
+	chatroom[i]++;
+	return i;
+}
+
+// Join a chat room and return the room joined on success. 
+	// Return -1 if the room is full.
+int join_chatroom(int room)
+{
+	if (chatroom[room] < CLIENTS_PER_ROOM) {
+		chatroom[room]++;
+		return room;
+	}
+	else return -1;
+}
+
+// LUKE
+// Broadcast a message ONLY within a particular chatroom.
+void chat_room_broadcast(int fromfd, char *message, int room) 
+{
+	// figure out sender address
+	struct sockaddr_in cliaddr;
+	socklen_t clen = sizeof(cliaddr);
+	if (getpeername(fromfd, (struct sockaddr*)&cliaddr, &clen) < 0) error("ERROR Unknown sender!");
+
+	// find the username associated with the sender
+	char* username;
+	int userfound = 0;
+	USR* cur = head;
+	while (cur != NULL) {
+		// check if cur is the one who sent the message
+		if (cur->clisockfd == fromfd) {
+			// set username if found
+			username = cur->username;
+			userfound = 1;
+		}
+		cur = cur->next;
+	}
+	if (userfound == 0) {
+		printf("Sender username not found.\n");
+	}
+
+	// traverse through all connected clients
+	cur = head;
+	while (cur != NULL) {
+		// check if cur is not the one who sent the message
+			// and is in the correct chatroom.
+		if (cur->clisockfd != fromfd && cur->chatroom == room) {
+			char buffer[512];
+
+			// prepare message
+			sprintf(buffer, "[room %d] : [%s] : [%s] : %s", room, username, inet_ntoa(cliaddr.sin_addr), message);
+			int nmsg = strlen(buffer);
+
+			// send!
+			int nsen = send(cur->clisockfd, buffer, nmsg, 0);
+			if (nsen != nmsg) error("ERROR send() failed");
+		}
+
+		cur = cur->next;
+	}
+}
+
+// LUKE
+// Complex communication procedure with client to determine chatroom.
+	// Returns -1 if no chatroom is requested. This occurs if the client 
+		// sends over the 's' character (standard broadcast procedure). 
+	// Otherwise returns the chat room number that this client should engage with.
+		// This occurs if the client send over a 'n' (create a new chatroom)
+		// or an chatroom number.
+int listen_for_chat_room(int clisockfd) 
+{
+	// Recieve the chat room request option from the client.
+		// Comes in form of an 's', 'n' or integer.
+	int nsen;
+	int temp;
+	char chatroom[1];
+	memset(chatroom, 0, 1);
+	int nrcv = recv(clisockfd, chatroom, 1, 0);
+	if (nrcv < 0) error("ERROR recv() failed");
+
+	// If option is an 's', return -1 for standard broadcast procedures.
+	if (chatroom[0] == 's') 
+		return -1;
+
+	// If option is an 'n', create a new chatroom and return its number.
+		// If the operation fails, will send an 'F' back to client, 
+			// otherwise will send the room number.
+	if (chatroom[0] == 'n') {
+		temp = create_new_chatroom();
+		chatroom[0] = temp == -1 ? 'F' : temp + '0';
+		nsen = send(clisockfd, chatroom, 1, 0);
+		if (nsen < 0) error("ERROR send() failed");
+	}
+
+	// If option is an int, join the specified chatroom and return its number.
+		// If the operation fails, will send an 'F' back to client, 
+			// otherwise will send the room number.
+	else if (isdigit(chatroom[0]) != 0) { 
+		temp = join_chatroom(atoi(&chatroom[0]));
+		chatroom[0] = temp == -1 ? 'F' : temp + '0';
+		nsen = send(clisockfd, chatroom, 1, 0);
+		if (nsen < 0) error("ERROR send() failed");
+	}
+	
+	// Return -1 for failures.
+	if (chatroom[0] == 'F') {
+		printf("Chat room joining failed.\n");	
+		return -1;
+	}
+
+	// Return the chatroom number for successfully joining.
+	printf("-----------------------------------------------------\n");
+	printf("Recieved a request for client, %d", clisockfd);
+	printf(" to join the chatroom %s\n", chatroom);
+	printf("-----------------------------------------------------\n");
+	return atoi(&chatroom[0]);
+}
 
 void* thread_main(void* args)
 {
@@ -151,7 +300,11 @@ void* thread_main(void* args)
 	// get socket descriptor from argument
 	int clisockfd = ((ThreadArgs*) args)->clisockfd;
 	char* username = ((ThreadArgs*) args)->username;
-	free(args);
+	// LUKE
+	// Perform the broadcast routine dictated by chat_status:
+		// -1 for broadcast to all, else a chatroom number.
+	int chat_status = ((ThreadArgs*) args)->chat_status;
+	free(args);	
 
 	//-------------------------------
 	// Now, we receive/send messages
@@ -163,8 +316,12 @@ void* thread_main(void* args)
 	if (nrcv < 0) error("ERROR recv() failed");
 
 	while (nrcv > 0) {
-		// we send the message to everyone except the sender
-		broadcast(clisockfd, buffer);
+		// LUKE
+		// Broadcast to all or broadcast to a particular chatroom.
+		if (chat_status == -1)
+			broadcast(clisockfd, buffer);
+		else
+			chat_room_broadcast(clisockfd, buffer, chat_status);
 
 		memset(buffer, 0, 256);
 		nrcv = recv(clisockfd, buffer, 255, 0);
@@ -211,19 +368,13 @@ int main(int argc, char *argv[])
 		printf("Connected: %s\n", inet_ntoa(cli_addr.sin_addr));
 		printf("Please wait while this new client connects.\n");
 
-		// Receive the username from the client, then create a LL entry
-		//     with its sockfd and username
+		// Receive the username from the client.
 		char username[16];
 		memset(username, 0, 16);
 		int nrcv = recv(newsockfd, username, 16, 0);
 		if (nrcv < 0) {
 			error("ERROR recv() failed");
 		}
-		// add this new client to the client list
-		add_tail(newsockfd, username);
-
-		// print the new list of clients now that another has been added
-		print_list();
 
 		// prepare ThreadArgs structure to pass client socket
 		ThreadArgs* args = (ThreadArgs*) malloc(sizeof(ThreadArgs));
@@ -231,6 +382,17 @@ int main(int argc, char *argv[])
 		
 		args->clisockfd = newsockfd;
 		args->username = username;
+		
+		// LUKE
+		// Complex communication procedure with client to determine chatroom.
+			// Gets a -1 for standard broadcast procedures, else a chatroom num.
+			// Please read the function documentation above.
+		int chat_status = listen_for_chat_room(newsockfd);
+		args->chat_status = chat_status;
+		
+		// Add to the tail of list and print.
+		add_tail(newsockfd, username, chat_status);
+		print_list();
 
 		pthread_t tid;
 		if (pthread_create(&tid, NULL, thread_main, (void*) args) != 0) error("ERROR creating a new thread");
